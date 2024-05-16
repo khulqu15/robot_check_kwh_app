b7170260cefb30e114755b3a974b6bc6ff37c741
Mohammad Khusnul Khuluq <59632952+khulqu15@users.noreply.github.com>
2024-05-17 02:04:03 +0900

    Update classification.py

--- processing/classification.py
+++ processing/classification.py
@@ -1,8 +1,10 @@
+import urllib.request
 import cv2
 import numpy as np
 import string
 import tensorflow as tf
 import os
+import urllib
 import sys
 
 alphabet = string.digits + string.ascii_lowercase + '.'
@@ -42,26 +44,36 @@ def main():
     interpreter = tf.lite.Interpreter(model_path=model_path)
     interpreter.allocate_tensors()
 
-    cap = cv2.VideoCapture(video_path)
+    # cap = cv2.VideoCapture(video_path)
 
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
+    while True:
+        try:
+            img_resp = urllib.request.urlopen(video_path)
+            imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
+            frame = cv2.imdecode(imgnp, -1)
+            # ret, frame = cap.read()
+            
+            if frame is None:
+                print("Failed to fetch frame from ESP32-CAM")
+                break
+            
+            roi = frame[y:y+h, x:x+w]
+
+            result = predict(roi, interpreter)
+            text = "".join(alphabet[index] for index in result[0] if index not in [blank_index, -1])
+            
+            cv2.putText(frame, f'Extracted text: {text}', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv2.imshow('Video', frame)
+            cv2.imshow('ROI', roi)
+
+            if cv2.waitKey(1) & 0xFF == 27:
+                break
+            
+        except Exception as e:
+            print(f"Error fetching frame: {e}")
             break
-
-        roi = frame[y:y+h, x:x+w]
-
-        result = predict(roi, interpreter)
-        text = "".join(alphabet[index] for index in result[0] if index not in [blank_index, -1])
         
-        cv2.putText(frame, f'Extracted text: {text}', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        cv2.imshow('Video', frame)
-        cv2.imshow('ROI', roi)
-
-        if cv2.waitKey(1) & 0xFF == 27:
-            break
-
-    cap.release()
+    # cap.release()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
